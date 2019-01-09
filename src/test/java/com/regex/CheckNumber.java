2de7451f037dbf3e446423a542cd3f9424b70562
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-01-09 17:46:36 +0100

    Add CheckNumber test using regex

---
+++ src/test/java/com/regex/CheckNumber.java
@@ -0,0 +1,31 @@
+package com.regex;
+
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class CheckNumber {
+
+    @Test
+    public void shouldTakeLastFiveDigits() {
+        assertTrue(matchStoreNumber("30243"));
+        assertFalse(matchStoreNumber("0"));
+    }
+
+    private static boolean matchStoreNumber(String input) {
+        String regex = "(\\d{5}$)";
+        Pattern p = Pattern.compile(regex);
+        Matcher matcher = p.matcher(input);
+        boolean isMatched = matcher.matches();
+       if (matcher.find()) {
+           return true;
+       }
+
+       return false;
+    }
+
+}
