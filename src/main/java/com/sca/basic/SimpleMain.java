f77723340710d59d4f18d9bda10ba09976302348
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-03-04 14:27:03 +0100

    Empty #main method

--- src/main/java/com/sca/basic/SimpleMain.java
+++ src/main/java/com/sca/basic/SimpleMain.java
@@ -4,14 +4,6 @@ public class SimpleMain {
 
 
     public static void main(String[] args) {
-        Boolean cond = false;
-
-        if (!Boolean.TRUE.equals(cond)) {
-            System.out.println("not true");
-        }
-        else {
-            System.out.println("true");
-        }
 
     }
 }
