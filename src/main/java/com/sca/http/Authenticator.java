fe6ceb228a9e703ee5b6a792c6f3bd061e077d8c
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-09-27 15:29:53 +0200

    Create Authenticator class

---
+++ src/main/java/com/sca/http/Authenticator.java
@@ -0,0 +1,14 @@
+package com.sca.http;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Authenticator {
+    String auth_token;
+}
