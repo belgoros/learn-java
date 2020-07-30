762256f33401282ccfac5d8b2509d55b7c5b91c9
Serguei Cambour <s.cambour@gmail.com>
2020-07-30 15:09:10 +0200

    Create MapToJsonConverter with Jackson XML

---
+++ src/main/java/com/sca/fasterxml/jackson/MapToJsonConverter.java
@@ -0,0 +1,18 @@
+package com.sca.fasterxml.jackson;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapToJsonConverter {
+    public static void main(String[] args) throws JsonProcessingException {
+        Map<String, String> map = new HashMap<>();
+        map.put("key", "value");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonResult = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+        System.out.println(jsonResult);
+    }
+}
