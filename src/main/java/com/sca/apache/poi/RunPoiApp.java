174409d0425e5847c257167ca669aa2ab2a973e3
Serguei Cambour <serguei.cambour@fednot.be>
2022-07-06 12:50:14 +0200

    Fix CellType case statement error

--- src/main/java/com/sca/apache/poi/RunPoiApp.java
+++ src/main/java/com/sca/apache/poi/RunPoiApp.java
@@ -118,11 +118,11 @@ public class RunPoiApp {
                     Cell cell = cellIterator.next();
 
                     switch (cell.getCellType()) {
-                        case Cell.CELL_TYPE_NUMERIC:
+                        case NUMERIC:
                             System.out.print((int) cell.getNumericCellValue()
                                     + "\t\t");
                             break;
-                        case Cell.CELL_TYPE_STRING:
+                        case STRING:
                             System.out.print(cell.getStringCellValue() + "\t\t");
                             break;
                     }
