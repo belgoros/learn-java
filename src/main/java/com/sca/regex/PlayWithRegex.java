0e28986129a340ab3c20eaf31367757fb5af9b37
Serguei Cambour <serguei.cambour@fednot.be>
2023-06-05 11:40:36 +0200

    Add a test for NRN regex

--- src/main/java/com/sca/regex/PlayWithRegex.java
+++ src/main/java/com/sca/regex/PlayWithRegex.java
@@ -6,11 +6,29 @@ import java.util.regex.Pattern;
 public class PlayWithRegex {
 
     public static void main(String[] args) {
+        checkLineFeedCarriageReturnAndOthers();
 
-        //checkNewton("haproxy[12107]: 127.0.0.1:37265 [07/Dec/2015:13:29:50.850]", "^haproxy\\[(\\d+)\\].*");
-        //checkNewton("toto yoyo 2 (12345)", "^(\\w+\\s+)+\\((\\d{5})\\)$");
-        checkContentRange();
+    }
 
+    private static void checkLineFeedCarriageReturnAndOthers() {
+        String input = "Vente par Mme SAUCE et Mme SCHMIT à Mr DETRY Daniel (D:2220917|374)" + "\n\r\f\tVos certificats PEB et électriques.";
+        String regexp = "^$|^[\\u000A\\u000D\\u000C\\u0009\\u0020-\\u007E\\u00A0-\\u02AF\\u20A0-\\u20BF]+$";
+        Pattern pattern = Pattern.compile(regexp);
+        Matcher m = pattern.matcher(input);
+        boolean isMatched = m.matches();
+        if (isMatched) {
+            System.out.println("The specified patter matches");
+        }
+        else {
+            System.out.println("Oups, no matches");
+        }
+
+    }
+
+    private static void checkNrn(String value) {
+        Pattern NRN_PATTERN = Pattern.compile("\\d{11}");
+        boolean matches = NRN_PATTERN.matcher(value).matches();
+        System.out.println("Match result: " + matches);
     }
 
     private static void checkNewton(String input, String pattern) {
@@ -18,7 +36,7 @@ public class PlayWithRegex {
         Matcher m = p.matcher(input);
         boolean isMatched = m.matches();
         if (isMatched) {
-            System.out.println("The specified pattern " + (isMatched ? "matches" : "does not match"));
+            System.out.println("The specified patter matches");
             System.out.println("matched group 1 -> " + m.group(1));
             System.out.println("matched group 2 -> " + m.group(2));
         } else {
@@ -35,13 +53,12 @@ public class PlayWithRegex {
 
         boolean isMatched = m.matches();
         if (isMatched) {
-            System.out.println("The specified pattern " + (isMatched ? "matches" : "does not match"));
+            System.out.println("The specified patter matches");
             System.out.println("matched group 1 -> " + m.group(1));
             System.out.println("matched group 2 -> " + m.group(2));
             System.out.println("matched group 3 -> " + m.group(3));
         } else {
             System.out.println("Oups, no matches");
         }
-
     }
 }
