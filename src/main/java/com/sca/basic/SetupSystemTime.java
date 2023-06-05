1b3c8e649ecbcf24e1640faef06b675c339889b3
Serguei Cambour <serguei.cambour@fednot.be>
2023-06-05 15:07:29 +0200

    Overload methods to take a duration

--- src/main/java/com/sca/basic/SetupSystemTime.java
+++ src/main/java/com/sca/basic/SetupSystemTime.java
@@ -14,26 +14,42 @@ public class SetupSystemTime {
         Instant now = Instant.now(clock);
         System.out.println("current instant in the past: " + now);
 
+        clock = runner.setToThePast(Duration.ofDays(-10));
+        now = Instant.now(clock);
+        System.out.println("When specified duration, current instant in the past: " + now);
+
         clock = runner.setToTheFuture();
         now = Instant.now(clock);
         System.out.println("current instant in the future: " + now);
+
+        clock = runner.setToTheFuture(Duration.ofDays(10));
+        now = Instant.now(clock);
+        System.out.println("when specified duration, current instant in the future: " + now);
     }
 
     /**
      * Rewind the clock back
      * @return a Clock in the past
      */
-    public Clock setToThePast() {
+    public Clock setToThePast(Duration duration) {
         Clock constantClock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
-        return Clock.offset(constantClock, Duration.ofDays(-10));
+        return Clock.offset(constantClock, duration);
+    }
+
+    public Clock setToThePast() {
+        return setToThePast(Duration.ofDays(-10));
     }
 
     /**
      * Go to the future
      * @return Clock in the future
      */
-    public Clock setToTheFuture() {
+    public Clock setToTheFuture(Duration duration) {
         Clock constantClock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
-        return Clock.offset(constantClock, Duration.ofDays(10));
+        return Clock.offset(constantClock, duration);
+    }
+
+    public Clock setToTheFuture() {
+        return setToTheFuture(Duration.ofDays(10));
     }
 }
