0eaf059760870af6a62f717f0f25a1dd9f2d78bc
belgoros <s.cambour@gmail.com>
2023-09-06 11:34:11 +0200

    Add the check for CLI arguments passed into the main class

--- src/main/java/com/sca/basic/SimpleMain.java
+++ src/main/java/com/sca/basic/SimpleMain.java
@@ -3,5 +3,10 @@ package com.sca.basic;
 public class SimpleMain {
 
     public static void main(String[] args) {
+        System.out.println("Hello from SimpleMain class");
+        System.out.println("There are "  + args.length + " arguments passed in");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
 }
