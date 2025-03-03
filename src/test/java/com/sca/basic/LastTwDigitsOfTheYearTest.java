1d7f7ca4fcb7a6e272d5aa939a1f1508e70aa83e
belgoros <s.cambour@gmail.com>
2025-03-03 09:10:00 +0100

    Add tests examples for last 2 digits of the year

---
+++ src/test/java/com/sca/basic/LastTwDigitsOfTheYearTest.java
@@ -0,0 +1,40 @@
+package com.sca.basic;
+
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+public class LastTwDigitsOfTheYearTest {
+    private final static String EXPECTED = Year.now().format(DateTimeFormatter.ofPattern("uu"));
+
+    @Test
+    public void whenUsingCalendarModulo_thenReturnsCorrectTwoDigitYear() {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int lastTwo = currentYear % 100;
+        String result = (lastTwo < 10) ? "0" + lastTwo : String.valueOf(lastTwo);
+
+        assertEquals(EXPECTED, result);
+    }
+
+    @Test
+    public void whenUsingSimpleDateFormat_thenReturnsCorrectTwoDigitYear() {
+        DateFormat df = new SimpleDateFormat("yy");
+        String result = df.format(new Date());
+
+        assertEquals(EXPECTED, result);
+    }
+
+    @Test
+    public void whenUsingJavaTimeAPI_thenReturnsCorrectTwoDigitYear() {
+        String result = Year.now().format(DateTimeFormatter.ofPattern("uu"));
+
+        assertEquals(EXPECTED, result);
+    }
+}
